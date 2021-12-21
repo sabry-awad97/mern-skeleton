0fde1c6365a3b7cd67d072a18759ddbb7a160931
Sabry Awad <dr.sabry1997@gmail.com>
2021-12-21 08:04:42 +0200

    These variables will give us the flexibility to change values from a single file and use it across the backend code.

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,11 @@
 const config = {
+    // To differentiate between development and production modes
     env: process.env.NODE_ENV || "development",
+    // To define the listening port for the server
     port: process.env.PORT || 3000,
+    // The secret key to be used to sign JWT
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    // The location of the MongoDB database instance for the project
     mongoUri:
         process.env.MONGODB_URI ||
         process.env.MONGO_HOST ||
