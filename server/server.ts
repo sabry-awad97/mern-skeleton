f8fac3374e4543761d7e3c492e16defba210d910
Sabry Awad <dr.sabry1997@gmail.com>
2021-12-21 09:20:00 +0200

    handling EADDRINUSE error

--- server/server.ts
+++ server/server.ts
@@ -15,4 +15,18 @@ server.on("listening", () => {
     console.info(`Server is running at http://localhost:${port}`);
 })
 
+server.on("error", (err: NodeJS.ErrnoException) => {
+    // EADDRINUSE => when another server is already
+    // listening on the requested port
 
+    // cmd => taskkill /F /IM node.exe
+    if (err.code === "EADDRINUSE") {
+        console.warn("Address already in use, retrying...");
+        setTimeout(() => {
+            server.close();
+            server.listen(port);
+        }, 1000);
+    } else {
+        console.error(err);
+    }
+});
