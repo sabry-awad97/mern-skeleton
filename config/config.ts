37887649906369e1fd10109854bfb35b51159122
Sabry Awad <dr.sabry1997@gmail.com>
2021-12-21 15:42:26 +0200

    sign-in a user

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@ const config = {
     // To define the listening port for the server
     port: process.env.PORT || 3000,
     // The secret key to be used to sign JWT
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    jwtSecret: process.env.JWT_SECRET || "YOUR_SeCrEt_Key",
     // The location of the MongoDB database instance for the project
     mongoUri:
         process.env.MONGODB_URI ||
--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -4,12 +4,38 @@ import User from "../models/user.model";
 import jwt from "jsonwebtoken";
 import expressJwt from "express-jwt";
 import config from "./../../config/config";
-const signin = (req: Request, res: Response) => {};
-const signout = (req: Request, res: Response) => {};
+
+const signin = async (req: Request, res: Response) => {
+    try {
+        let user = await User.findOne({ email: req.body.email as string });
+        if (!user) return res.status(401).json({ error: "User not found" });
+        if (!user.authenticate(req.body.password as string)) {
+            return res
+                .status(401)
+                .send({ error: "Email and password don't match." });
+        }
+        const token = jwt.sign({ _id: user._id }, config.jwtSecret);
+        res.cookie("t", token, {
+            expires: new Date(Date.now() + 9999),
+        });
+        return res.json({
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch (err) {
+        return res.status(401).json({ error: "Could not sign in" });
+    }
+};
+
 const requireSignin = expressJwt({
     algorithms: ["HS256"],
     secret: config.jwtSecret,
     userProperty: "auth",
 });
+
 const hasAuthorization = (req: Request, res: Response) => {};
 export default { signin, signout, requireSignin, hasAuthorization };
