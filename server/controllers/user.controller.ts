9d8b9d32d20c07f3bc848ac9656dd0f86faeaea8
Sabry Awad <dr.sabry1997@gmail.com>
2021-12-21 13:05:21 +0200

    Creating a new user

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,8 +1,24 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 
 import User from "../models/user.model";
+import errorHandler from "./../helpers/dbErrorHandler";
 
-const create: RequestHandler = (req, res, next) => {};
+/**
+ * This function creates a new user with the user JSON object
+ * that's received in the POST request from the frontend within req.body.
+ */
+const create: RequestHandler = async (req, res, next) => {
+    try {
+        await User.create(req.body);
+        return res.status(200).json({
+            message: "Successfully signed up!",
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err),
+        });
+    }
+};
 const list: RequestHandler = (req, res) => {};
 const userByID = (
     req: Request,
